d044d755d426d7663ff0008ccdaf826b569b5ffe
emil <emilbabazade29@gmail.com>
2020-11-29 14:47:45 +0200

    removed extra count up and reset, changed dice number to dice image, double dice!

--- app/src/main/java/emil/babazade/diceroller/MainActivity.kt
+++ app/src/main/java/emil/babazade/diceroller/MainActivity.kt
@@ -4,52 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rollButton: Button
-    private lateinit var resultTextView: TextView
-    private lateinit var countUpButton: Button
-    private lateinit var resetButton: Button
+    private lateinit var diceImage: ImageView
+    private lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultTextView = findViewById(R.id.result_text)
         rollButton = findViewById(R.id.roll_button)
-        countUpButton = findViewById(R.id.count_up_button)
-        resetButton = findViewById(R.id.reset_button)
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
 
         rollButton.setOnClickListener(::handleRollClick)
-        countUpButton.setOnClickListener(::handleCountUpClick)
-        resetButton.setOnClickListener(::handleResetClick)
     }
 
     private fun handleRollClick(view: View) {
-        val randomInt = (1..6).random()
-        resultTextView.text = randomInt.toString()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
     }
 
-    private fun handleCountUpClick(view: View) {
-        var lastRoll = try {
-            resultTextView.text.toString().toInt()
-        } catch (e: NumberFormatException) {
-            0
-        }
-
-        lastRoll = if(lastRoll == 6) lastRoll else lastRoll + 1
-
-        resultTextView.text = lastRoll.toString()
+    private fun getRandomDiceImage(): Int = when((1..6).random()) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
     }
 
-    private fun handleResetClick(view: View) {
-        try {
-            resultTextView.text.toString().toInt()
-        } catch (e: java.lang.NumberFormatException) {
-            return
-        }
-        resultTextView.text = "0"
-    }
 }
