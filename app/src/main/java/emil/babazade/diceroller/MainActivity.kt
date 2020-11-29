7e74d939280615b8f89edce5fe9de5c2e4d9f25b
emil <emilbabazade29@gmail.com>
2020-11-29 13:16:58 +0200

    done

--- app/src/main/java/emil/babazade/diceroller/MainActivity.kt
+++ app/src/main/java/emil/babazade/diceroller/MainActivity.kt
@@ -2,10 +2,54 @@ package emil.babazade.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rollButton: Button
+    private lateinit var resultTextView: TextView
+    private lateinit var countUpButton: Button
+    private lateinit var resetButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.result_text)
+        rollButton = findViewById(R.id.roll_button)
+        countUpButton = findViewById(R.id.count_up_button)
+        resetButton = findViewById(R.id.reset_button)
+
+        rollButton.setOnClickListener(::handleRollClick)
+        countUpButton.setOnClickListener(::handleCountUpClick)
+        resetButton.setOnClickListener(::handleResetClick)
+    }
+
+    private fun handleRollClick(view: View) {
+        val randomInt = (1..6).random()
+        resultTextView.text = randomInt.toString()
+    }
+
+    private fun handleCountUpClick(view: View) {
+        var lastRoll = try {
+            resultTextView.text.toString().toInt()
+        } catch (e: NumberFormatException) {
+            0
+        }
+
+        lastRoll = if(lastRoll == 6) lastRoll else lastRoll + 1
+
+        resultTextView.text = lastRoll.toString()
+    }
+
+    private fun handleResetClick(view: View) {
+        try {
+            resultTextView.text.toString().toInt()
+        } catch (e: java.lang.NumberFormatException) {
+            return
+        }
+        resultTextView.text = "0"
     }
 }
